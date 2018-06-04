d4510934f976ab25295f7d93b9166002eeddf63e
user <user@user-PC>
2018-06-04 15:39:50 +0900

    ChangeLogBean.java
    変更ログのBean作った

---
+++ src/model/entity/ChangeLogBean.java
@@ -0,0 +1,78 @@
+package model.entity;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+/**
+ * 変更ログBean
+ *
+ * @author 大川
+ *
+ */
+public class ChangeLogBean implements Serializable {
+	private Date updateDate;
+	private String userId;
+	private String section;
+	private String operation;
+	private String empCode;
+	/**
+	 * @return updateDate 編集日時
+	 */
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+	/**
+	 * @param updateDate セットする updateDate
+	 */
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+	/**
+	 * @return userId ユーザID
+	 */
+	public String getUserId() {
+		return userId;
+	}
+	/**
+	 * @param userId セットする userId
+	 */
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	/**
+	 * @return section 部署名
+	 */
+	public String getSection() {
+		return section;
+	}
+	/**
+	 * @param section セットする section
+	 */
+	public void setSection(String section) {
+		this.section = section;
+	}
+	/**
+	 * @return operation 操作
+	 */
+	public String getOperation() {
+		return operation;
+	}
+	/**
+	 * @param operation セットする operation
+	 */
+	public void setOperation(String operation) {
+		this.operation = operation;
+	}
+	/**
+	 * @return empCode　従業員コード
+	 */
+	public String getEmpCode() {
+		return empCode;
+	}
+	/**
+	 * @param empCode セットする empCode
+	 */
+	public void setEmpCode(String empCode) {
+		this.empCode = empCode;
+	}
+}
